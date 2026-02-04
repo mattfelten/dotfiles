05389f18243d38e4e5555ddcbc6cf92bc8edb10c
Matt Felten <m@mattfelten.com>
2026-02-04 11:25:27 -0800

    remove time machine. it doesn't work

--- macos-defaults/system.sh
+++ macos-defaults/system.sh
@@ -7,9 +7,6 @@
 # Prevent Time Machine from prompting to use new hard drives as backup volume
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
-# Disable local Time Machine backups
-hash tmutil &> /dev/null && sudo tmutil disablelocal
-
 ###############################################################################
 # Activity Monitor                                                            #
 ###############################################################################
